28ed30d6d109fea7cbc2926278f93f5299256e31
AL PJ <145345908+thesoftwareengr@users.noreply.github.com>
2023-11-16 15:19:26 +0000

    doneImplementation

---
+++ dice-game/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import RollDice from "./Components/RollDice";
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
---
+++ dice-game/src/Components/Die.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Die = ({ face }) => {
+  return <i className={`fas fa-dice-${face}`}></i>;
+};
+
+export default Die;
