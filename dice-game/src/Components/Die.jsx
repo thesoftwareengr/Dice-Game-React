6ad2e312f1f5b0b23b2aaf8fa426182160a416c8
AL PJ <145345908+thesoftwareengr@users.noreply.github.com>
2023-11-17 16:30:23 +0800

    addedSpin

--- dice-game/src/Components/Die.jsx
+++ dice-game/src/Components/Die.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Die = ({ face }) => {
-  return <i className={`fas fa-dice-${face}`}></i>;
+  return <i className={`fa-solid fa-dice-${face} fa-2xl`}></i>;
 };
 
 export default Die;
--- dice-game/src/Components/RollDice.jsx
+++ dice-game/src/Components/RollDice.jsx
@@ -1,39 +1,42 @@
-import React, { useState } from 'react';
-import Die from './Die';
+  import React, { useState } from 'react';
+  import Die from './Die';
+  import '../RollDice.css'
 
+  const RollDice = () => {
+    const faces = ['one', 'two', 'three', 'four', 'five', 'six'];
+    const [die1, setDie1] = useState('one');
+    const [die2, setDie2] = useState('one');
+    const [rolling, setRolling] = useState(false);
 
-const RollDice = () => {
-  const faces = ['one', 'two', 'three', 'four', 'five', 'six'];
-  const [die1, setDie1] = useState('one');
-  const [die2, setDie2] = useState('one');
-  const [rolling, setRolling] = useState(false);
+    const roll = () => {
+      if (!rolling) {
+        setRolling(true);
 
-  const roll = () => {
-    if (!rolling) {
-      setRolling(true);
+        setTimeout(() => {
+          const newDie1 = faces[Math.floor(Math.random() * faces.length)];
+          const newDie2 = faces[Math.floor(Math.random() * faces.length)];
+          setDie1(newDie1);
+          setDie2(newDie2);
+          setRolling(false);
+        }, 1000);
+      }
+    };
 
-      // Simulate dice rolling
-      setTimeout(() => {
-        const newDie1 = faces[Math.floor(Math.random() * faces.length)];
-        const newDie2 = faces[Math.floor(Math.random() * faces.length)];
-        setDie1(newDie1);
-        setDie2(newDie2);
-        setRolling(false);
-      }, 1000);
-    }
+    return (
+      <div className="RollDice">
+        <div className={`DiceContainer ${rolling ? 'rolling' : ''}`}>
+        
+          <Die className={`DiceContainer ${rolling ? 'rolling' : ''}`} face={die1} />
+        </div>
+        <div className={`DiceContainer ${rolling ? 'rolling' : ''}`}>
+          <Die className={`DiceContainer ${rolling ? 'rolling' : ''}`} face={die2} />
+        </div>
+        <button className='button' onClick={roll} disabled={rolling}>
+          {rolling ? 'Rolling...' : 'Roll Dice'}
+        </button>
+
+      </div>
+    );
   };
 
-  return (
-    <div className="RollDice">
-      <div className="DiceContainer">
-        <Die face={die1} />
-        <Die face={die2} />
-      </div>
-      <button onClick={roll} disabled={rolling}>
-        {rolling ? 'Rolling...' : 'Roll Dice'}
-      </button>
-    </div>
-  );
-};
-
-export default RollDice;
+  export default RollDice;
